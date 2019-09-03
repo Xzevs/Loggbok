a9e565eafbbf6af008b82a342bb8ca4177d53573
rasmus.karlsson11 <rasmus.karlsson@elev.ga.ntig.se>
2019-09-03 10:43:03 +0200

    Lade till olika alternativ

--- src/loggbok.java
+++ src/loggbok.java
@@ -1,2 +1,21 @@
 public class loggbok {
+    public static void main(String[] args) {
+        printMenu();
+
+
+    }
+
+    private static void printMenu() {
+        System.out.println("1. Visa loggbok \n"
+                + "2. Lägg till i loggbok \n"
+                + "3. Redigera \n"
+                + "4. Spara \n"
+                + "5. Läs in \n"
+                +"6. Avsluta \n "
+
+        ) ;
+
+
+
+    }
 }
