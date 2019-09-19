6c43c744748414987ae44c9d1e5b254490e3e721
rasmus.karlsson11 <rasmus.karlsson@elev.ga.ntig.se>
2019-09-19 13:59:31 +0200

    Började att göra en arraylist

--- src/loggbok.java
+++ src/loggbok.java
@@ -1,12 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class loggbok {
     public static void main(String[] args) {
-        printMenu();
+        ArrayList<LogEntry> post =new ArrayList<LogEntry>();
+        Scanner tgb = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            printMenu();
+            running = choose (post, tgb);
+
+        }
+
 
 
     }
 
     private static void printMenu() {
-        System.out.println("1. Visa loggbok \n"
+        System.out.println("Välj:"+"\n"
+                + "1. Visa loggbok \n"
                 + "2. Lägg till i loggbok \n"
                 + "3. Redigera \n"
                 + "4. Spara \n"
